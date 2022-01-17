ba35e915721eb97cc9e5e49ab10dcd151efb946c
Jeferson-Labout <jefson1989@gmail.com>
2022-01-17 01:54:24 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -3,15 +3,24 @@ import MovieCard from 'components/MovieCard'
 // import MovieScore from 'components/MovieScore'
 // import MovieStars from 'components/MovieStars'
 import Pagination from 'components/Paginaton'
+import { useState, useEffect } from 'react'
+import { MoviePage } from 'types/movie'
 import { BASE_URL } from 'utils/request'
 
 function Listing() {
-  // forma errada
-  axios.get(`${BASE_URL}/movies`).then(response => {
-    console.log(response.data)
-  })
+  const [pageNumber, setPageNumber] = useState(0)
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}/movies?size=12&page=10`).then(response => {
+      const data = response.data as MoviePage
+      console.log(data)
+      setPageNumber(data.number)
+    })
+  }, [])
+
   return (
     <>
+      <p>{pageNumber}</p>
       <Pagination />
       <div className="container">
         <div className="row">
