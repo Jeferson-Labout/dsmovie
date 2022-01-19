6614d4a28fb17c686fc16de216fa96576bb99946
Jeferson-Labout <jefson1989@gmail.com>
2022-01-19 01:23:58 -0300

    Pagination0

--- frontend/src/components/MovieStars/index.tsx
+++ frontend/src/components/MovieStars/index.tsx
@@ -5,7 +5,7 @@ import {ReactComponent as StarHalf} from 'assets/img/star_half.svg';
 import {ReactComponent as StarEmpty} from 'assets/img/star_empty.svg';
 
 import './styles.css';
-import { type } from 'os';
+
 
 type Props = {
   score: number;
