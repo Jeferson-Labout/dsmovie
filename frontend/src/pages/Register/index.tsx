393bdee1868bcd52a4ca9641bfe37b5b0124a2d5
jeferson <jeferson@matriz.maxdata>
2022-01-22 15:58:24 -0300

    cadastro dos filmes

--- frontend/src/pages/Register/index.tsx
+++ frontend/src/pages/Register/index.tsx
@@ -1,24 +1,55 @@
-function Register(){
 
-    return(
 
-<form>
-  <div className="form-control">
-    <label htmlFor="formGroupExampleInput">Informe o titulo do Filme</label>
-    <input type="text" className="form-control" id="formGroupExampleInput"/>
-  </div>
-  <div className="form-control">
-    <label htmlFor="formGroupExampleInput2">Informe a URL da Imagem</label>
-    <input type="text" className="form-control" id="formGroupExampleInput2" />
-  </div>
-  <div className="dsmovie-form-btn-container">
-            <button type="submit" className="btn btn-primary dsmovie-btn">
-              Salvar
-            </button>
-          </div>
-</form>
+import { useNavigate } from 'react-router-dom'
+import axios, { AxiosRequestConfig } from "axios";
+import './styles.css'
+import { BASE_URL } from 'utils/request'
 
-    );
+
+
+function Register() {
+  const navigate = useNavigate();
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const title = (event.target as any).title.value;
+    const imagem = (event.target as any).imagem.value;
+
+
+    const config: AxiosRequestConfig = {
+      baseURL: BASE_URL,
+      method: 'POST',
+      url: '/movies',
+      data: {
+        title: title,
+        image: imagem
+      }
+    }
+    axios(config).then((response) => {
+console.log(response.data)
+
+      navigate("/")
+    })
+
+  }
+  return (
+    <div className="dsmovie-form-container">
+      <form className="dsmovie-form" onSubmit={handleSubmit}>
+        <div className="form-group dsmovie-form-group">
+          <label htmlFor="title">Informe o titulo do Filme</label>
+          <input type="text" className="form-control" id="title" />
+        </div>
+        <div className="form-group dsmovie-form-group">
+          <label htmlFor="imagem">Informe a URL da Imagem</label>
+          <input type="text" className="form-control" id="imagem" />
+        </div>
+        <div className="dsmovie-form-btn-container">
+          <button type="submit" className="btn btn-primary dsmovie-btn">
+            Salvar
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default Register;
